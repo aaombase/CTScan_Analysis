736b9c8541aae2a77de10c27fa6bf98c2619b654
Developer <aaombase@gmail.com>
2026-04-30 00:30:41 +0530

    chore: Remove debug files from repository

--- backend/debug_analysis.js
+++
@@ -1,2 +0,0 @@
-import analysisRouter from "./routes/analysis.js";
-console.log("Analysis Router loaded");
--- backend/debug_auth.js
+++
@@ -1,3 +0,0 @@
-import { authenticateToken, requireRole } from "./middleware/auth.js";
-console.log("authenticateToken:", typeof authenticateToken);
-console.log("requireRole:", typeof requireRole);
